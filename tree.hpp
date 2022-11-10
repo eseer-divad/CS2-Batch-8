0b458b0b5eba54886bd0bae87e6a932f29da9aa1
eseer-divad <93809439+eseer-divad@users.noreply.github.com>
2022-11-10 05:52:07 -0500

    First Commit

---
+++ tree.hpp
@@ -0,0 +1,133 @@
+/*
+   COPYRIGHT (C) 2017 Student Name (UANET ID ) All rights reserved.
+   CS assignment
+   Author.  Student Name
+            zippy@zips.uakron.edu
+   Version. 1.01 09.09.2017
+   Purpose: This program ...
+*/
+#ifndef TREE_HPP
+#define TREE_HPP
+#include <iostream>
+#include <algorithm>
+#include "Treenode.hpp"
+
+// Tree class-template definition
+template<typename NODETYPE> class Tree {
+public:
+   // insert node in Tree
+   void insertNode(const NODETYPE& value) {
+      insertNodeHelper(&rootPtr, value);
+   }
+
+   // begin preorder traversal of Tree
+   void preOrderTraversal() const {
+      preOrderHelper(rootPtr);
+   }
+
+   // begin inorder traversal of Tree
+   void inOrderTraversal() const {
+      inOrderHelper(rootPtr);
+   }
+
+   // begin postorder traversal of Tree
+   void postOrderTraversal() const {
+      postOrderHelper(rootPtr);
+   }
+
+   // get the depth of the tree
+   int getDepth() const {
+      int totalDepth{0};
+      int currentDepth{0};
+
+      determineDepth(rootPtr, &totalDepth, &currentDepth);
+      return totalDepth;
+   }
+
+   // begin binary search
+   TreeNode<NODETYPE>* binaryTreeSearch(int val) const {
+      return binarySearchHelper(rootPtr, val);
+   }
+
+private:
+   TreeNode<NODETYPE>* rootPtr{nullptr};
+
+   // utility function called by insertNode; receives a pointer
+   // to a pointer so that the function can modify pointer's value
+   void insertNodeHelper(
+      TreeNode<NODETYPE>** ptr, const NODETYPE& value) {
+      // subtree is empty; create new TreeNode containing value
+      if (*ptr == nullptr) {
+         *ptr = new TreeNode<NODETYPE>(value);
+      }
+      else { // subtree is not empty
+             // data to insert is less than data in current node
+         if (value <= (*ptr)->data) {
+            insertNodeHelper(&((*ptr)->leftPtr), value);
+         }
+         else {
+            insertNodeHelper(&((*ptr)->rightPtr), value);
+         }
+      }
+   }
+
+   // utility function to perform preorder traversal of Tree
+   void preOrderHelper(TreeNode<NODETYPE>* ptr) const {
+      if (ptr != nullptr) {
+         std::cout << ptr->data << ' '; // process node
+         preOrderHelper(ptr->leftPtr); // traverse left subtree
+         preOrderHelper(ptr->rightPtr); // traverse right subtree
+      }
+   }
+
+   // utility function to perform inorder traversal of Tree
+   void inOrderHelper(TreeNode<NODETYPE>* ptr) const {
+      if (ptr != nullptr) {
+         inOrderHelper(ptr->leftPtr); // traverse left subtree
+         std::cout << ptr->data << ' '; // process node
+         inOrderHelper(ptr->rightPtr); // traverse right subtree
+      }
+   }
+
+   // utility function to perform postorder traversal of Tree
+   void postOrderHelper(TreeNode<NODETYPE>* ptr) const {
+      if (ptr != nullptr) {
+         postOrderHelper(ptr->leftPtr); // traverse left subtree
+         postOrderHelper(ptr->rightPtr); // traverse right subtree
+         std::cout << ptr->data << ' '; // process node
+      }
+   }
+
+   // calculate the depth of the tree
+   int determineDepth(TreeNode<NODETYPE>* parentPtr) {
+
+    if (parentPtr == NULL)
+        return 0;
+    else {
+        // Find the height of left, right subtrees
+        int left_depth = determineDepth(parentPtr->left);
+        int right_depth = determineDepth(parentPtr->right);
+        // Display max depth.
+        std::cout << "Depth: " + (std::max(left_depth, right_depth) + 1);
+        return 0;
+      }
+   }
+
+   // do a binary search on the Tree
+   TreeNode<NODETYPE>* binarySearchHelper(TreeNode<NODETYPE>* ptr, NODETYPE data) {
+
+    // Base Cases: root is null or data is present at root
+    if (ptr == NULL || ptr->data == data)
+       return ptr;
+
+    // Key is greater than root's data
+    if (ptr->data < data)
+       return search(ptr->right, data);
+
+    // Key is smaller than root's data
+    return search(ptr->left, data);
+   }
+
+};
+
+#endif // TREE_HPP
