a2143842e0307f81dd255640c926e9070ba5e3be
David Reese <dcr54@uakron.edu>
2022-11-27 00:31:42 -0500

    added assignment 10 prompt

---
+++ ASSIGN10/recursion.cpp
@@ -0,0 +1,97 @@
+/*
+   COPYRIGHT (C) 2022 David Reese (dcr54@uakron.edu) All rights reserved.
+   CS assignment 10
+*/
+
+#include <iostream>
+
+// prototypes
+template <class T>
+void printarray(const T[], int);
+void chew_system_stack();
+
+/*
+ * Recursive version of fibonacci
+ */
+int fibrecurse(int n) {
+
+}
+
+/*
+ * Iterative (loop) version of fibonacci
+ */
+int fibiterate(int n) {
+
+}
+
+/*
+ * Recursive quicksort
+ */
+template <class T>
+void quicksort(T ar[], int first, int last) {
+
+}
+
+int main() {
+    const int FIBLOOPS = 10;
+    const int ARSIZ = 16;
+    int ar[ARSIZ];
+
+    std::cout << "Recursive fib: ";
+    for(int i = 0; i < FIBLOOPS; ++i)
+    {
+        std::cout << fibrecurse(i) << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "Iterative fib: ";
+    for(int i = 0; i < FIBLOOPS; ++i)
+    {
+        std::cout << fibiterate(i) << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Quicksort:" << std::endl;
+    // populate random array
+    for(int i = 0; i < ARSIZ; ++i)
+    {
+        ar[i] = (i * 3037) % 2099;  // prime numbers! again!
+    }
+    printarray(ar, ARSIZ);
+    quicksort(ar, 0, ARSIZ-1);
+    printarray(ar, ARSIZ);
+
+    //std::cout << "Chewing system stack:" << std::endl;
+    //chew_system_stack();
+
+    return 0;
+}
+
+/*  utility functions, do not modify  */
+template <class T>
+void printarray(const T ar[], int s)
+{
+    for(int i = 0; i < s; ++i)
+    {
+        std::cout << ar[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+void printstackpointer()
+{
+    void * stack_pointer;
+    // inline assembly language to get stack pointer
+    __asm__ __volatile__("movl  %%esp,%%eax"
+                             :"=a"(stack_pointer)
+                             );
+    std::cout << stack_pointer << std::endl;
+}
+
+void chew_system_stack()
+{
+    // Feel free to play with this, but please
+    // do NOT enable this for your final commit!
+    printstackpointer();
+    chew_system_stack();
+}
+/* end utilities */
